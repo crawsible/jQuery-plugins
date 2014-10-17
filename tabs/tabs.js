ddd91eb83bf3d757f57f88f7e59ef3a758f3d221
Colin Jackson <colin.s.jackson90@gmail.com>
2014-10-16 23:59:27 -0400

    Show and hide related content on link click

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,5 +1,20 @@
 $.Tabs = function (el) {
+  this.$el = $(el);
+  this.$contentTabs = $(this.$el.data("content-tabs"));
+  this.$activeTab = this.$contentTabs.find(".active");
 
+  this.$el.on("click", "a", this.clickTab.bind(this));
+};
+
+$.Tabs.prototype.clickTab = function (event) {
+  event.preventDefault();
+
+  var $activatedA = $(event.currentTarget);
+  $activatedA.closest("ul").find(".active").removeClass("active");
+  $activatedA.addClass("active");
+
+  this.$activeTab.removeClass("active");
+  this.$activeTab = $($activatedA.attr("href")).addClass("active");
 };
 
 $.fn.tabs = function (el) {
