a01b9ed075b38306ac811a2e4b978d0273f41bef
Colin Jackson <colin.s.jackson90@gmail.com>
2014-10-17 01:37:14 -0400

    Move through image list with prev/next links

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,5 +1,37 @@
 $.Carousel = function (el) {
   this.$el = $(el);
+  this.activeIdx = 0;
+  this.imgAtActiveIdx().addClass("active");
+  this.bindEvents();
+};
+
+$.Carousel.prototype.bindEvents = function () {
+  this.$el.find(".slide").on("click", function (event) {
+    if (this.$el.find(".slide-left").is(event.target)) {
+      this.slide(-1);
+    } else if (this.$el.find(".slide-right").is(event.target)) {
+      this.slide(1);
+    }
+  }.bind(this));
+};
+
+$.Carousel.prototype.imgAtIndex = function (idx) {
+  return this.$el.find(".items *").eq(idx);
+};
+
+$.Carousel.prototype.imgAtActiveIdx = function () {
+  return this.imgAtIndex(this.activeIdx);
+};
+
+$.Carousel.prototype.imgCount = function () {
+  return this.$el.find(".items *").length;
+};
+
+$.Carousel.prototype.slide = function (dir) {
+  this.imgAtActiveIdx().removeClass("active");
+  this.activeIdx += dir;
+  this.activeIdx = (this.activeIdx + this.imgCount()) % this.imgCount();
+  this.imgAtActiveIdx().addClass("active");
 };
 
 $.fn.carousel = function () {
